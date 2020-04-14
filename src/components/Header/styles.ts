9c1daf5af4fca1d7aba55ca6359bfce598589135
hiagopsilva <hiagohps360@gmail.com>
2020-04-14 00:45:46 -0300

    added component Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 60px;
+  background: #7159C1;
+  color: #FFF;
+  display: center;
+  align-items: center;
+  padding: 0 30px;
+`;
